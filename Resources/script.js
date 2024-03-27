750e6352ac688f033fa2dac81ce0d59370b23795
Véronique Cartier <156599884+Cnicquel@users.noreply.github.com>
2024-03-27 07:22:34 +0100

    third commit

--- Resources/script.js
+++ Resources/script.js
@@ -1,5 +1,17 @@
-let tl = gsap.timeline({repeat: -1, repeatDelay: 1, yoyo: true})
+document.addEventListener("DOMContentLoaded", (event) => {
+   
+
+    let tl = gsap.timeline({repeat: -1, repeatDelay: 3, yoyo: true})
 
 tl.to(".t1", { rotation: 360 });
 tl.to(".t2", { rotation: 360 });
 tl.to(".t3", { rotation: 360 });
+tl.to(".t4", { rotation: 360 });
+tl.to(".t5", { rotation: 360 });
+
+
+
+
+ 
+
+});
