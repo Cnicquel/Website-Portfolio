dfa3b6a7f4ad1b98e4bd665772488024668de4cb
Véronique Cartier <156599884+Cnicquel@users.noreply.github.com>
2024-03-28 11:08:02 +0100

    Add interactivity and contact me

--- Resources/script.js
+++ Resources/script.js
@@ -9,6 +9,9 @@ tl.to(".t3", { rotation: 360 });
 tl.to(".t4", { rotation: 360 });
 tl.to(".t5", { rotation: 360 });
 
-
+gsap.to("#hi", {text:"Hi, I am Véronique Cartier", ease:"power1.in", duration:"3", repeat:-1, yoyo:true, repeatDelay:12});
+gsap.to("#sk", {text:"Skills", ease:"power1.in", duration:"3", repeat:-1, yoyo:true, repeatDelay:0.6});
+gsap.to("#pt", {text:"Portfolio", ease:"power1.in", duration:"3", repeat:-1, yoyo:true, delay:"3", repeatDelay:0.6});
+gsap.to("#ct", {text:"Contact", ease:"power1.in", duration:"3", repeat:-1, yoyo:true, repeatDelay:0.6});
 });
 
